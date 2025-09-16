34f63be38a9b32383f4027e82ce8e1339563195e
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2025-09-15 20:38:51 -0400

    Carta al lector and social media included

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Libro de relatos';
+export const SITE_DESCRIPTION = 'Libro de relatos';
